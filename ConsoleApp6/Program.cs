da7972a2d62e298fe0af883cc29ed781797d7a79
Sepideh <sepideh.badran05@gmail.com>
2024-03-17 19:33:47 +0330

    add files

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+namespace six
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first num");
+            int first =int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second num");
+            int second =int.Parse(Console.ReadLine());
+            int sum = 1;
+            for (int i = 1; i <= second; i++)
+            {
+                sum *= first;
+                {
+                    Console.WriteLine(sum.ToString());
+                }
+               
+            }
+        }
+    }
+}
